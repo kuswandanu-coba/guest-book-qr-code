3b1a9dd92262cb064cb0f10ba18aef8b9611de2f
kuswandanu <kuswandanu@gmail.com>
2019-06-17 22:48:18 +0700

    1

---
+++ precache-manifest.ad6342d995891b37f88d067f34027f6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a339eb1a5ffd2c866a36884228e045",
+    "url": "/index.html"
+  },
+  {
+    "revision": "247ad091aac74628352e",
+    "url": "/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "97f2f12308ca43217386",
+    "url": "/static/js/2.51bb3ea5.chunk.js"
+  },
+  {
+    "revision": "247ad091aac74628352e",
+    "url": "/static/js/main.eb9e7429.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
